35baef166de1515caef5c4e2a4d303827a486e01
Diuri <diurivj@icloud.com>
2018-07-14 13:39:28 -0500

    done

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const passport     = require('passport');
+const session      = require('express-session');
+const MongoStore   = require('connect-mongo')(session);
 
 mongoose.Promise = Promise;
 mongoose
@@ -24,6 +26,19 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+app.use(session({
+  secret: "$3cr3t_putit@",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection:mongoose.connection,
+    ttl: 30 * 24 * 60 * 60 //30 días
+  }),
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -31,28 +46,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
-
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-const express = require('express');
-const router  = express.Router();
+const express          = require('express');
+const router           = express.Router();
+const fetch            = require('node-fetch');
+const passportFacebook = require('../helpers/facebook');
 
-/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/movies', (req, res, next) => {
+  fetch('https://api.themoviedb.org/3/genre/28/movies?api_key=82b5663c68f2ad62437a48269129ca36&language=es&include_adult=false&sort_by=created_at.asc')
+  .then(results => results.json())
+  .then(movies => {
+    res.render('movies', movies); 
+  });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const id =  req.params;
+  fetch(`https://api.themoviedb.org/3/movie/${id.id}?api_key=82b5663c68f2ad62437a48269129ca36&language=en_US`)
+  .then(result => result.json())
+  .then(movie => {
+    console.log(movie);
+    res.render('detail', movie);
+  })
+});
+
+router.get('/facebook', passportFacebook.authenticate('facebook'));
+
+router.get('/facebook/callback',
+  passportFacebook.authenticate('facebook', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/movies');
+});
+
 module.exports = router;
+
